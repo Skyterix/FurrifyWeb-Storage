3f4e47b1c156085be2a9b2fb47eef1b1ec5c32c6
Sky <skyterix1991@gmail.com>
2021-09-16 21:27:29 +0200

    Login emulation

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  signIn(): void {
+    this.router.navigate(['/']);
+  }
+
+  signUp(): void {
+    this.router.navigate(['/']);
   }
 
 }
