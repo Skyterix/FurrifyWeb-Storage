3035d4a545ebbc251a4d26c939401d131744d23a
Sky <skyterix1991@gmail.com>
2021-09-20 19:22:52 +0200

    User menu

--- src/app/posts/nav/nav.component.ts
+++ src/app/posts/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {faCaretDown} from "@fortawesome/free-solid-svg-icons/faCaretDown";
 import {KeycloakService} from "keycloak-angular";
 
@@ -14,11 +14,22 @@ export class NavComponent implements OnInit {
     isMenuOpened = false;
     username!: string;
 
+    @ViewChild('menu')
+    menuRef!: ElementRef;
+
     constructor(private keycloakService: KeycloakService) {
     }
 
     ngOnInit() {
         this.username = this.keycloakService.getUsername();
+    }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: Event) {
+        if (this.isMenuOpened && !this.menuRef.nativeElement.contains(event.target)) {
+            this.toggleMenu();
+        }
+
     }
 
     toggleMenu(): void {
