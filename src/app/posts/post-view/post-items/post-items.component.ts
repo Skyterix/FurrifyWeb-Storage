cc36a54b02cb7e62a579a2d9ceb8783748a9840a
Sky <skyterix1991@gmail.com>
2021-10-02 20:10:36 +0200

    Prevent loading same media

--- src/app/posts/post-view/post-items/post-items.component.ts
+++ src/app/posts/post-view/post-items/post-items.component.ts
@@ -1,29 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Media} from "../../../shared/model/media.model";
 import {MediaUtils} from "../../../shared/util/media.utils";
 import {Post} from "../../../shared/model/post.model";
 import {Store} from "@ngrx/store";
 import * as fromApp from "../../../store/app.reducer";
 import {selectMedia} from "../../store/posts.actions";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-post-items',
     templateUrl: './post-items.component.html',
     styleUrls: ['./post-items.component.css']
 })
-export class PostItemsComponent implements OnInit {
+export class PostItemsComponent implements OnInit, OnDestroy {
     @Input() post!: Post;
 
     sortedMedia!: Media[];
+
+    selectedMedia!: Media | null;
+
+    private storeSubscription!: Subscription;
 
     constructor(private store: Store<fromApp.AppState>) {
     }
 
     ngOnInit(): void {
+        this.storeSubscription = this.store.select('posts').subscribe(state => {
+            this.selectedMedia = state.selectedMedia;
+        });
+
         this.sortedMedia = MediaUtils.sortByPriority([...this.post.mediaSet]);
     }
 
+    ngOnDestroy(): void {
+        this.storeSubscription.unsubscribe();
+    }
+
     onLoadMediaRequest(media: Media): void {
+        if (media == this.selectedMedia) {
+            return;
+        }
+
         this.store.dispatch(selectMedia({
             media: media
         }));
