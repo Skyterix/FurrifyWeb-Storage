975676c849973521214b617321be75d753fb738c
Sky <skyterix1991@gmail.com>
2021-11-05 23:41:27 +0100

    Dynamic background

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,25 +1,43 @@
-import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    Renderer2,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
 import {Subscription} from "rxjs";
 import {PostCreateService} from "./post-create/post-create.service";
 import {PostCreateComponent} from "./post-create/post-create.component";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Store} from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import {Post} from "../shared/model/post.model";
+import {MediaUtils} from "../shared/util/media.utils";
 
 @Component({
     selector: 'app-posts',
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit, OnDestroy {
+export class PostsComponent implements OnInit, OnDestroy, AfterViewInit {
 
     @ViewChild('createPost', {read: ViewContainerRef}) createPostRef!: ViewContainerRef;
+    @ViewChild('background', {read: ElementRef}) backgroundRef!: ElementRef;
+
+    private selectedPost!: Post | null;
 
     private postCreateOpenSubscription!: Subscription;
     private postCreateCloseSubscription!: Subscription;
+    private storeSubscription!: Subscription;
 
-    constructor(private postCreateService: PostCreateService,
-                private componentFactoryResolver: ComponentFactoryResolver,
+    constructor(private store: Store<fromApp.AppState>,
+                private postCreateService: PostCreateService,
                 private router: Router,
-                private activatedRoute: ActivatedRoute) {
+                private activatedRoute: ActivatedRoute,
+                private renderer: Renderer2) {
     }
 
     ngOnInit(): void {
@@ -37,6 +55,16 @@ export class PostsComponent implements OnInit, OnDestroy {
         }
     }
 
+    ngAfterViewInit(): void {
+        this.storeSubscription = this.store.select('posts').subscribe(state => {
+            // If selected post changed
+            if (this.selectedPost !== state.selectedPost) {
+                this.selectedPost = state.selectedPost;
+
+                this.loadBackground();
+            }
+        });
+    }
 
     ngOnDestroy(): void {
         this.postCreateOpenSubscription.unsubscribe();
@@ -51,10 +79,8 @@ export class PostsComponent implements OnInit, OnDestroy {
             queryParamsHandling: "merge"
         });
 
-        const postCreateComponent = this.componentFactoryResolver.resolveComponentFactory(PostCreateComponent);
-
         this.createPostRef!.clear();
-        this.createPostRef!.createComponent(postCreateComponent);
+        this.createPostRef!.createComponent(PostCreateComponent);
     }
 
     private clearPostCreateForm(): void {
@@ -66,6 +92,20 @@ export class PostsComponent implements OnInit, OnDestroy {
         });
 
         this.createPostRef.clear();
+    }
+
+    private loadBackground(): void {
+        if (!!this.selectedPost) {
+            const media = MediaUtils.getHighestPriorityMedia(this.selectedPost.mediaSet);
+            if (!media) {
+                return;
+            }
+
+            this.renderer.setStyle(this.backgroundRef.nativeElement, "background-image", "url(" + media.thumbnailUrl + ")")
+            this.renderer.setStyle(this.backgroundRef.nativeElement, "opacity", "1")
+        } else {
+            this.renderer.setStyle(this.backgroundRef.nativeElement, "opacity", "0")
+        }
     }
 
 }
