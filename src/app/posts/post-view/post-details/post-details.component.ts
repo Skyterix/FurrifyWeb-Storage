40aff018e47a48e55af51692bc09f795456d8c51
Sky <skyterix1991@gmail.com>
2021-11-21 20:51:57 +0100

    Photoswipe gallery

--- src/app/posts/post-view/post-details/post-details.component.ts
+++ src/app/posts/post-view/post-details/post-details.component.ts
@@ -10,6 +10,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {CDN_ADDRESS} from "../../../shared/config/api.constants";
 import {PostsService} from "../../posts.service";
 import {QueryPost} from "../../../shared/model/query/query-post.model";
+import * as PhotoSwipe from "photoswipe";
+import * as PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
 
 @Component({
     selector: 'app-post-details',
@@ -30,6 +32,8 @@ export class PostDetailsComponent implements OnInit {
     sortedTags!: Tag[];
     sortedMedia!: Media[];
 
+    galleryItems: { src: string, w: number, h: number }[] = [];
+
     constructor(private renderer: Renderer2,
                 private postsService: PostsService,
                 private store: Store<fromApp.AppState>,
@@ -41,6 +45,13 @@ export class PostDetailsComponent implements OnInit {
         // Sort
         this.sortedTags = TagUtils.sortTagsByPriority([...this.post.tags]);
         this.sortedMedia = MediaUtils.sortByPriority([...this.post.mediaSet]);
+        this.sortedMedia.forEach(media => {
+            this.galleryItems.push({
+                src: CDN_ADDRESS + media.fileUri,
+                w: 0,
+                h: 0
+            });
+        });
 
         // On index change
         this.activatedRoute.params.subscribe(params => {
@@ -72,6 +83,33 @@ export class PostDetailsComponent implements OnInit {
         setTimeout(() => {
             this.postsService.triggerSearch();
         });
+    }
+
+    openImageGallery(): void {
+        const pswpElement: HTMLElement = document.querySelector('.pswp')!;
+
+        const options = {
+            history: false,
+            index: 0,
+            clickToCloseNonZoomable: false
+        };
+
+        const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.galleryItems, options);
+        gallery.listen('gettingData', (index, item) => {
+            if (item.w! < 1 || item.h! < 1) {
+                const img = new Image();
+                img.onload = function () {
+                    // @ts-ignore
+                    item.w = this.width;
+                    // @ts-ignore
+                    item.h = this.height;
+                    gallery.invalidateCurrItems();
+                    gallery.updateSize(true);
+                };
+                img.src = item.src!;
+            }
+        });
+        gallery.init();
     }
 
     private loadMedia(media: Media): void {
