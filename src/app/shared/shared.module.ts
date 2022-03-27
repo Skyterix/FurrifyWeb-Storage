f6c831b698a0e9c8f79243389a085ff331ca9fd0
Sky <skyterix1991@gmail.com>
2022-03-28 00:23:57 +0200

    Source url directive
    Signed-off-by: Sky <skyterix1991@gmail.com>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import {NgModule} from '@angular/core';
 import {ColoredTagDirective} from "./directive/colored-tag.directive";
 import {RemoveTextPipe} from "./pipe/remove-text.pipe";
 import {AccountLinkDirective} from './directive/account-link.directive';
+import {SourceToHrefDirective} from './directive/source-to-href.directive';
 
 @NgModule({
     declarations: [
         ColoredTagDirective,
         RemoveTextPipe,
-        AccountLinkDirective
+        AccountLinkDirective,
+        SourceToHrefDirective
     ],
     exports: [
         ColoredTagDirective,
         RemoveTextPipe,
-        AccountLinkDirective
+        AccountLinkDirective,
+        SourceToHrefDirective
     ],
     imports: []
 })
