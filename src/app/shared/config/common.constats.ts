ecb66d468a9e1879ab5abb88703649ba6f0311d1
Sky <skyterix1991@gmail.com>
2022-03-28 19:43:14 +0200

    New deviant art strategy support
    Signed-off-by: Sky <skyterix1991@gmail.com>

--- src/app/shared/config/common.constats.ts
+++ src/app/shared/config/common.constats.ts
@@ -1,3 +1,4 @@
 // Extracted extension will be in [1] position in array.
 export const EXTENSION_EXTRACT_REGEX = /(?:\.([^.]+))?$/;
 export const FILENAME_REGEX = /^[a-zA-Z0-9](?:[a-zA-Z0-9() ._-]*[a-zA-Z0-9() ._-])?\.[a-zA-Z0-9_-]+$/;
+export const URL_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)$/;
