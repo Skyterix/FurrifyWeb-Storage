6eac66da13408b7e3115faa3a2e754158c018973
Sky <skyterix1991@protonmail.com>
2022-07-21 08:40:53 +0200

    Set correct values for constants

--- src/app/shared/config/common.constats.ts
+++ src/app/shared/config/common.constats.ts
@@ -8,5 +8,5 @@ export const TAG_REGEX = /^[a-zA-Z0-9_-]*$/;
 export const TAG_VALUE_MAX_LENGTH = 64;
 export const ARTIST_NICKNAME_MAX_LENGTH = 256;
 
-export const MAX_TAGS_IN_POST = 3;
-export const MAX_ARTISTS_IN_POST = 3;
+export const MAX_TAGS_IN_POST = 256;
+export const MAX_ARTISTS_IN_POST = 256;
